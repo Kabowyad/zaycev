0a50f08212df679e0a15b429f033a6454f5f3e04
Yan Yukhnovets <kabowyad@gmail.com>
2017-06-27 12:24:53 +0500

    Поправил тест

--- test/SortTest.scala
+++ test/SortTest.scala
@@ -2,20 +2,23 @@ import entities.File
 import org.specs2.mutable._
 import play.api.test.WithApplication
 import service._
+import play.api.test._
+
 /**
   * Created by user on 18/06/17.
   */
 
-class SortTest extends Specification with DiskReaders with DiskReaderComponentImpl
-  with SortersFiles with SortFilesFromDiskImpl {
+class SortTest extends Specification
+  with SortersFiles
+  with SortFilesFromDiskImpl {
     "SortFiles" should {
 
-        val input = "\"total 559576\n     14 First\n    68 Second\n 11920 Third\n     4 zina\n\""
-        val ordered = List[File](File("Third", 11920), File("Second", 68), File("First", 14), File("zina", 4))
+        "sort fine" in new WithApplication {
+            val input = "\"total 559576\n     14 First\n    68 Second\n 11920 Third\n     4 zina\n\""
+            val ordered = List[File](File("Third", 11920), File("Second", 68), File("First", 14), File("zina", 4))
+            val sort = sortFiles.sort(input)
 
-        "sort fine" in {
-            val list = sortFilesFromDisk(input)
-            list must_== ordered
+            sort must_== ordered
         }
     }
 }
