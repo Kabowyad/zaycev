9f20d4b0768c2d6c0874b3dc8d4a9e4f2aca019e
Yan Yukhnovets <kabowyad@gmail.com>
2017-06-27 12:26:42 +0500

    close #3

--- test/SortTest.scala
+++ test/SortTest.scala
@@ -17,7 +17,6 @@ class SortTest extends Specification
             val input = "\"total 559576\n     14 First\n    68 Second\n 11920 Third\n     4 zina\n\""
             val ordered = List[File](File("Third", 11920), File("Second", 68), File("First", 14), File("zina", 4))
             val sort = sortFiles.sort(input)
-
             sort must_== ordered
         }
     }
